d134d343eeeddf0715f0966fdf44d799adae7aeb
huanglelele <872194944@qq.com>
2018-06-13 15:30:28 +0800

    商品列表页面

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,31 @@ var sass = require("gulp-sass");
 
 
 
-gulp.task("sasss",function(){
-	gulp.src(["scss/index.scss"])
+gulp.task("suoyouscss",function(){
+	gulp.src(["scss/**/*"])
 	.pipe(sass())
 	.pipe(gulp.dest("D:\\php\\WWW\\ehaier02\\css"));
 });
-gulp.task("dengluscss",function(){
-	gulp.src(["scss/denglu.scss"])
-	.pipe(sass())
-	.pipe(gulp.dest("D:\\php\\WWW\\ehaier02\\css"));
-});
-gulp.task("zhucescss",function(){
-	gulp.src(["scss/zhuce.scss"])
-	.pipe(sass())
-	.pipe(gulp.dest("D:\\php\\WWW\\ehaier02\\css"));
-});
+// gulp.task("sasss",function(){
+// 	gulp.src(["scss/index.scss"])
+// 	.pipe(sass())
+// 	.pipe(gulp.dest("D:\\php\\WWW\\ehaier02\\css"));
+// });
+// gulp.task("dengluscss",function(){
+// 	gulp.src(["scss/denglu.scss"])
+// 	.pipe(sass())
+// 	.pipe(gulp.dest("D:\\php\\WWW\\ehaier02\\css"));
+// });
+// gulp.task("zhucescss",function(){
+// 	gulp.src(["scss/zhuce.scss"])
+// 	.pipe(sass())
+// 	.pipe(gulp.dest("D:\\php\\WWW\\ehaier02\\css"));
+// });
+// gulp.task("shangpinliebiaoscss",function(){
+// 	gulp.src(["scss/goodslist.scss"])
+// 	.pipe(sass())
+// 	.pipe(gulp.dest("D:\\php\\WWW\\ehaier02\\css"));
+// });
 
 
 
@@ -34,6 +44,10 @@ gulp.task("copyhtml",function(){
 gulp.task("zhucehtml",function(){
 	gulp.src("zhuce.html").pipe(gulp.dest("D:\\php\\WWW\\ehaier02"));
 });
+gulp.task("shangpinliebiaohtml",function(){
+	gulp.src("goodslist.html").pipe(gulp.dest("D:\\php\\WWW\\ehaier02"));
+});
+
 
 
 
@@ -65,6 +79,7 @@ gulp.task("js",function(){
 
 
 gulp.task("watchall",function(){
+	gulp.watch("scss/**/*",["suoyouscss"]);
 	gulp.watch("scss/index.scss",["sasss"]);
 	gulp.watch("scss/denglu.scss",["dengluscss"]);
 	gulp.watch("index.html",["copyfile"]);
@@ -75,4 +90,6 @@ gulp.task("watchall",function(){
 	gulp.watch("scss/zhuce.scss",["zhucescss"]);
 	gulp.watch("denglu.php",["dengluphp"]);
 	gulp.watch("zhuce.php",["zhucephp"]);
+	gulp.watch("goodslist.html",["shangpinliebiaohtml"]);
+	gulp.watch("goodslist.scss",["shangpinliebiaoscss"]);
 });
